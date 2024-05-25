8f21a409efcec41b6d0ee6e9e43b37d618e1b41e
2004Goyal <124761230+2004Goyal@users.noreply.github.com>
2024-05-25 18:46:52 +0530

    Add files via upload

---
+++ utils/data.js
@@ -0,0 +1,37 @@
+import {
+  FitnessCenterRounded,
+  LocalFireDepartmentRounded,
+  TimelineRounded,
+} from "@mui/icons-material";
+
+export const counts = [
+  {
+    name: "Calories Burned",
+    icon: (
+      <LocalFireDepartmentRounded sx={{ color: "inherit", fontSize: "26px" }} />
+    ),
+    desc: "Total calories burned today",
+    key: "totalCaloriesBurnt",
+    unit: "kcal",
+    color: "#FFAA29",
+    lightColor: "#000000",
+  },
+  {
+    name: "Workouts",
+    icon: <FitnessCenterRounded sx={{ color: "inherit", fontSize: "26px" }} />,
+    desc: "Total no of workouts for today",
+    key: "totalWorkouts",
+    unit: "",
+    color: "#FFAA29",
+    lightColor: "#000000",
+  },
+  {
+    name: "Average  Calories Burned",
+    icon: <TimelineRounded sx={{ color: "inherit", fontSize: "26px" }} />,
+    desc: "Average Calories Burned on each workout",
+    key: "avgCaloriesBurntPerWorkout",
+    unit: "kcal",
+    color: "#FFAA29",
+    lightColor: "#000000",
+  },
+];
