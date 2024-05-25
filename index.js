4031e4cbd71dbd713a5fdf6cd9858f62b8a8f9e8
2004Goyal <124761230+2004Goyal@users.noreply.github.com>
2024-05-25 18:51:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./redux/store";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./routes/User.js";
 
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true })); // for form data
+
+app.use("/api/user/", UserRoutes);
+// error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
 });
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-  
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <ThemeProvider theme={darkTheme}>
-          <App />
-        </ThemeProvider>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
-);
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "Hello developers from GFG",
+  });
+});
+
+const connectDB = () => {
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect('mongodb+srv://goyal:crystal12@cluster0.nugauwh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => console.log("Connected to Mongo DB"))
+    .catch((err) => {
+      console.error("failed to connect with mongo");
+    });
+};
+ const startServer = async () => {
+  try {
+    connectDB();
+    app.listen(8080, () => console.log("Server started on port 8080"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
